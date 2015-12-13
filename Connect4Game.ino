e7b4fe7b1e4d17fbb4159555183d370cebde124a
Alan Evans <thealanevans@googlemail.com>
2015-12-13 22:35:36 +0000

    Criss-cross pattern

--- Connect4Game.ino
+++ Connect4Game.ino
@@ -26,6 +26,11 @@ void setup() {
   display[4][3] = RED;
   display[5][3] = GREEN;
   display[6][5] = ORANGE;
+
+  for(int x=0;x<8;x++)
+    for(int y=0;y<8;y++){
+      display[x][y] = ((x+y)%2==0)?RED:GREEN;
+   }
 }
 
 void drawDisplay() {
