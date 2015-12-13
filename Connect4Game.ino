54e8856ba5656d43987a7f4195242b2284e5743b
Alan Evans <thealanevans@googlemail.com>
2015-12-13 22:22:35 +0000

    Way to draw display and prevent tearing

--- Connect4Game.ino
+++ Connect4Game.ino
@@ -6,6 +6,9 @@ int clockPin = 4;
 int dataPin = 7;
 
 int toggle;
+int drawColumn;
+
+int display[8][8];
 
 void setup() {
   //set pins to output so you can control the shift register
@@ -14,36 +17,41 @@ void setup() {
   pinMode(dataPin, OUTPUT);
 }
 
-void loop() {
-  // count from 0 to 255 and display the number 
-  // on the LEDs
-  for (int col = 0; col < 8; col++) {
-    // take the latchPin low so 
-    // the LEDs don't change while you're sending in bits:
-    digitalWrite(latchPin, LOW);
-    delay(50);
-    // shift out the bits:    
-
-    int green = 1<<col;
-    int red = 1<<col;
+void drawDisplay() {
+  int col = drawColumn;
+  // take the latchPin low so 
+  // the LEDs don't change while you're sending in bits:
+  digitalWrite(latchPin, LOW);
+  delay(50);
+  // shift out the bits:    
+  
+  int green = 1<<col;
+  int red = 1<<col;
     
-    //green
-    shiftOut(dataPin, clockPin, MSBFIRST, ~green);
+  //green
+  shiftOut(dataPin, clockPin, MSBFIRST, ~green);
     
-    //red
-    shiftOut(dataPin, clockPin, MSBFIRST, ~red);
+  //red
+  shiftOut(dataPin, clockPin, MSBFIRST, ~red);
     
-    //column
-    shiftOut(dataPin, clockPin, MSBFIRST, 1<<col);
+  //column
+  shiftOut(dataPin, clockPin, MSBFIRST, 1<<col);
     //take the latch pin high so the LEDs will light up:
-    digitalWrite(latchPin, HIGH);
-    delayMicroseconds(100);
-    digitalWrite(13, toggle==0?HIGH:LOW);
-    toggle=(toggle+1)%2;
-    //digitalWrite(latchPin, LOW);
-    //shiftOut(dataPin, clockPin, MSBFIRST, 255);
-    //shiftOut(dataPin, clockPin, MSBFIRST, 255);
-    //shiftOut(dataPin, clockPin, MSBFIRST, 1<<row);
-    //digitalWrite(latchPin, HIGH);
+  digitalWrite(latchPin, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(13, toggle==0?HIGH:LOW);
+  toggle=(toggle+1)%2;
+  drawColumn=(drawColumn+1)%8;
+}
+
+void syncDisplay() {
+  while(drawColumn!=0){
+    drawDisplay();
   }
 }
+
+void loop() {
+  drawDisplay();
+  syncDisplay();
+}
+
