e49ed129e4762961badf4dc4a8d6bc3761a2c16c
Alan Evans <thealanevans@googlemail.com>
2016-03-07 21:21:01 +0000

    Formatting

--- Connect4Game/buttons.c
+++ Connect4Game/buttons.c
@@ -35,7 +35,7 @@ int readSingleButton(ButtonState *state, long timeMs, ButtonStates *states, int 
   return result;
 }
 
-int readButtons(ButtonStates * states, long timeMs) {
+int readButtons(ButtonStates *states, long timeMs) {
   int result = 0;
   result |= readSingleButton(&states->left, timeMs, states, BTN_DOWN_LEFT, input_left);
   result |= readSingleButton(&states->right, timeMs, states, BTN_DOWN_RIGHT, input_right);
