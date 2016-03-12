afa9e523c98451f451eb3e77858d0b7c004c99ad
Alan Evans <thealanevans@googlemail.com>
2016-03-12 09:34:13 +0000

    Fixed winChecker for verticles

--- Connect4Game/winchecker.c
+++ Connect4Game/winchecker.c
@@ -8,7 +8,7 @@ WinChecker *winChecker;
 WinChecker *getWinChecker() {
   if (winChecker)
     return winChecker;
-    
+
   winChecker = calloc(1, sizeof(WinChecker));
   init_wins(winChecker);
   return winChecker;
@@ -27,11 +27,11 @@ int fastCheckVertWin(Board *board, Board *resultBoard, int column) {
   uint64_t data = getData(board);
   for (int y = 0; y < CONNECT4_HEIGHT - 3; y++) {
     uint64_t mask = checker->vMask;
-    mask >>= IDX(column, y);
-    if (mask & data == mask) {
+    mask = mask << IDX(column, y);
+    if ((mask & data) == mask) {
       result++;
       for (int i = 0; i < 4; i++)
-        mark(resultBoard, column, i);
+        mark(resultBoard, column, i + y);
     }
   }
   return result;
@@ -41,7 +41,7 @@ void init_wins(WinChecker *checker) {
   p("Creating win checker");
   Board *temp = createBoard();
   checker->vMask = createVertWinMask(temp);
-  p("Done win checker");
+  print64(checker->vMask, "vmask");
   free(temp);
 }
 
